2c8746622147c83d831f0a056defb04d2e16d0d3
Claire Rhodes <claire.rhodes13@gmail.com>
2024-11-03 15:21:25 -0800

    added lose condition and play again functionality

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 /* Query Selectors */
-const unicornBoba = document.querySelector("#unicorn"); // select unicorn picture
 const bobaCounter = document.querySelector("#counter"); // defining boba counter
-const makeBobaButton = document.querySelector("#button");
+const makeBobaButton = document.querySelector("#boba");
 const wlStatement = document.querySelector("#wlstatement"); // statement that shows up if you win/lose
+const playButton = document.querySelector("#start"); // select my start button on title screen
+const playAgainButton = document.querySelector("#restart");
+const dontClickMe = document.querySelector("#combust");
 
 // console.dir(unicornBoba); // checking that i properly selected the unicorn
 // console.dir(bobaCounter); // checking that i properly selected the counter
 
+/* Variables */
 let totalBoba = 0; // set my boba amount to 0
 
-// add in a win scenario
-makeBobaButton.addEventListener('click', () => { // create function that does something when i click on my Add Boba button
-    totalBoba = totalBoba + 1; // add 1 to my count and replace count with new total
-    bobaCounter.textContent = `Total Boba: ${totalBoba}`; // change the boba counter text to show current amount of total boba
+/* Event Listeners */
+if (playButton) {
+    playButton.addEventListener('click', () => {
+        // link me to my game page
+        window.location.href = "./index.html";
+    });
+};
 
-    if (totalBoba >= 11) { // win condition! if reach 11 total boba then 
+if (makeBobaButton) {
+    playAgainButton.style.visibility = "hidden"; // google'd to find this syntax
+    playAgainButton.addEventListener ('click', () => {
+        // refresh my page which restarts counter
+        location.reload();
+    });
+    makeBobaButton.addEventListener('click', () => { // does something when i click on my Add Boba button
+        totalBoba = totalBoba + 1; // add 1 to my count and replace count with new total
+        bobaCounter.textContent = `Total Boba: ${totalBoba}`; // change the boba counter text to show current amount of total boba
+    
+        if (totalBoba >= 11) { // win condition!
+            makeBobaButton.disabled = true;
+            wlStatement.innerText = 'You won! Game Over';
+            playAgainButton.style.visibility = "visible";
+        }
+    });
+};
+
+if (dontClickMe) { // lose condition!
+    dontClickMe.addEventListener('click', () => {
         makeBobaButton.disabled = true;
-        wlStatement.innerText = 'You won! Game Over'
-    // } else if (/* timer runs out*/) {
-    //     makeBobaButton.disabled = true;
-    //     wlStatement.innerText = 'You didnt make enough boba before time ran out. Game Over'
-    }
-});
+        wlStatement.innerText = 'I told you not to click me muahahhaaha GGs';
+        playAgainButton.style.visibility = "visible";
+    });
+};
+
+
+
+
+
 
